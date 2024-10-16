6a25d9e4c17a5a01fd13f08f0f71a667688f0980
ryanb <ryan.bilac2010@hotmail.com>
2024-10-16 13:31:44 -0300

    second commit

--- config/plugins.js
+++ config/plugins.js
@@ -14,5 +14,10 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET'),
+      },
+    },
     // ...
   });
